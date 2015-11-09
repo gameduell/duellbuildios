4dfdcff4360f0b4c82cbf25c3fa210a311a577a1
Rui Campos <rui.campos@gameduell.de>
2015-11-09 10:01:49 +0100

    changed default simulator os to 9, since it is the most common now

--- duell/build/plugin/platform/PlatformConfiguration.hx
+++ duell/build/plugin/platform/PlatformConfiguration.hx
@@ -149,7 +149,7 @@ class PlatformConfiguration
 			DEBUG : false,
 			SIMULATOR : false,
 			SIM_DEVICE : 'iPhone-4s',
-			SIM_OS : '8.2',
+			SIM_OS : '9.0',
 			OUTPUT_FILE : "",
 			IOS_VERSION : "",
 
